244cfc7018c3e522554feb9848e09d4e34a82301
Valentina Brambila Grando <vgrando3@gatech.edu>
2020-08-11 18:24:30 -0400

    Todo-List first edition

---
+++ assets/js/todos.js
@@ -0,0 +1,26 @@
+// Check off todos on click
+$("ul").on("click", "li", function(){
+    $(this).toggleClass("completed");
+});
+
+$("ul").on("click","span",function(event){
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove;
+    });
+    event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+    if (event.which === 13){
+        var todoText = $(this).val();
+        $(this).val("");
+        $("ul").prepend("<li><span><i class='far fa-trash-alt'></i></span> "+todoText+"</li>");
+    }
+});
+
+$(".fa-plus").click(function(){
+    $("input[type='text']").fadeToggle();
+});
+
+
+
